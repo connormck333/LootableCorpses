c9a8738a5f6a8bd7a81129fbb54caee9c2bb54e6
Connor McKenzie <connormckenzie@mysite.local>
2024-10-30 21:18:13 +0000

    Support for 1.20-1.21

--- src/main/java/com/devConnor/lootableCorpses/utils/ConfigManager.java
+++ src/main/java/com/devConnor/lootableCorpses/utils/ConfigManager.java
@@ -5,11 +5,9 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigManager {
 
-    private static LootableCorpses lootableCorpses;
     private static FileConfiguration config;
 
     public static void setupConfig(LootableCorpses lootableCorpses) {
-        ConfigManager.lootableCorpses = lootableCorpses;
         ConfigManager.config = lootableCorpses.getConfig();
 
         lootableCorpses.saveDefaultConfig();
